ed2b444a19056da95f79758216ab6742beaeb390
Aishwarya <Aishwaryalodhi2014@gmail.com>
2022-11-02 03:16:41 +0000

    Aishwarya

---
+++ .cmpe283-1.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/aishwaryalodhi2014/cmpe283-1/cmpe283-1.mod := { echo  /home/aishwaryalodhi2014/cmpe283-1/cmpe283-1.o;  echo; } > /home/aishwaryalodhi2014/cmpe283-1/cmpe283-1.mod
---
+++ cmpe283-1.mod
@@ -0,0 +1,2 @@
+/home/aishwaryalodhi2014/cmpe283-1/cmpe283-1.o
+
